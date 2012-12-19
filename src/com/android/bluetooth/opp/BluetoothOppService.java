d724f2f85df7e1503a3e46474dbc39209b3118e7
Vadivel Thekkamalai <vthekk@codeaurora.org>
2012-12-19 14:48:39 +0530

    Bluetooth: Close cursor object to fix the leak
    CRs-Fixed: 422871
    Change-Id: I789eb6238d23170d47a91f85022690bfbd38411f

--- src/com/android/bluetooth/opp/BluetoothOppService.java
+++ src/com/android/bluetooth/opp/BluetoothOppService.java
@@ -940,6 +940,7 @@ public class BluetoothOppService extends Service {
                              WHERE_INBOUND_INTERRUPTED_ON_POWER_OFF, null);
                 if (V) Log.v(TAG, "Delete aborted inbound share, number = " + delNum);
             }
+            cursorToFile.close();
         }
 
         // Only keep the inbound and successful shares for LiverFolder use
